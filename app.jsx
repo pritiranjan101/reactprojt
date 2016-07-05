1ff4b5ef71e08a7edd5c09fdb8645973dffe63bb
SIPU <SIPU>
2016-07-05 14:35:23 +0530

    initial commit

---
+++ app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./container/header";
+//import Content from "./container/content";
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <Header/>
+       
+         </div>
+      );
+   }
+}
+
+export default App;
---
+++ container/content.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Router, Route, Link, browserHistory } from 'react-router'
+
+export default class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>Content</h2>
+            <p>The content text!!!
+
+            We want React Router to be a 
+            stable dependency that’s easy 
+            to keep current. We follow the
+             same versioning as 
+
+            </p>
+         </div>
+      );
+   }
+}
+
+
+export default Content;
+
+
+
